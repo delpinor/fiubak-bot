36e40493fb138841d88403888a59a3b85d7ff919
delpinor <delpinor@gmail.com>
2021-11-29 20:43:14 -0300

    feat: Se envia chat_id a la API usuarios

--- spec/usuario_parser_spec.rb
+++ spec/usuario_parser_spec.rb
@@ -3,7 +3,7 @@ require_relative '../app/helpers/usuario_parser.rb'
 
 describe 'Usuario parser' do
   let(:mensaje) { 'Juan,33454678, juan@gmail.com' }
-  let(:usuario) { UsuarioParser.new.a_json(mensaje) }
+  let(:usuario) { UsuarioParser.new.a_json(mensaje, 3) }
 
   it 'Al parsear tengo el dni' do
     expect(JSON.parse(usuario)['dni']).to eq 33_454_678
