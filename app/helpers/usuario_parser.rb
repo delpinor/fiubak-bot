3f663eb41cefa7d2c25c3e72b5fcd464e88ec2d5
delpinor <delpinor@gmail.com>
2021-11-29 21:51:07 -0300

    fix: test no pusheados

--- app/helpers/usuario_parser.rb
+++ app/helpers/usuario_parser.rb
@@ -2,7 +2,7 @@ class UsuarioParser
   def a_json(mensaje, id_chat)
     datos_array = mensaje.split(',')
     {
-      id_chat: id_chat,
+      id: id_chat,
       nombre: datos_array[0].strip,
       dni: datos_array[1].to_i,
       email: datos_array[2].strip
