b2659a0bda98ea024e3abbf33c1917fdee3904a7
delpinor <delpinor@gmail.com>
2021-11-30 18:24:42 -0300

    refactor: se ordena el codigo en los test del bot

--- spec/comandos_bot_spec.rb
+++ spec/comandos_bot_spec.rb
@@ -7,45 +7,33 @@ require_relative '../spec/helpers/bot_helpers'
 describe 'Bot de telegram' do
   it 'Cuando le envio /start, entonces el bot response Hola' do
     token = 'fake_token'
-
     cuando_envio_un_mensaje(token, '/start')
     entonces_obtengo_el_mensaje(token, 'Hola, Nairobi')
-
     app = BotClient.new(token)
-
     app.run_once
   end
 
   it 'Cuando le envio hola, entonces el bot response Hola' do
     token = 'fake_token'
-
     cuando_envio_un_mensaje(token, 'hola')
     entonces_obtengo_el_mensaje(token, 'Hola Nairobi')
-
     app = BotClient.new(token)
-
     app.run_once
   end
 
   it 'Cuando le envio /registrar al bot entonces obtengo un mensaje exitoso ' do
     token = 'fake_token'
-
     cuando_registro_un_usuario(token, '/registrar Juan, 9999, jua@gmail.com')
     entonces_obtengo_el_mensaje(token, 'registro exitoso')
-
     app = BotClient.new(token)
-
     app.run_once
   end
 
   it 'cuando envio /ayuda entonces obtengo una lista donde veo /registrar Nombre, DNI, email' do
     token = 'fake_token'
-
     cuando_envio_un_mensaje(token, '/ayuda')
     entonces_obtengo_el_mensaje(token, 'Comandos disponibles:' + "\n" + '/registrar Nombre, DNI, email' + "\n" + '/nueva_venta marca, modelo, año, patente')
-
     app = BotClient.new(token)
-
     app.run_once
   end
 end
