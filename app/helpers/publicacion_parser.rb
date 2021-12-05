dc6553db07c997de0cb26c4f5b972ae43df7866a
lotero1 <lotero@fi.uba.ar>
2021-12-05 19:14:26 -0300

    test: Agrego test para leer input de post de publicacion por p2p
    Signed-off-by: lotero1 <lotero@fi.uba.ar>

--- app/helpers/publicacion_parser.rb
+++ app/helpers/publicacion_parser.rb
@@ -2,7 +2,8 @@ class PublicacionParser
   def a_json(mensaje)
     datos_array = mensaje.split(',')
     {
-      id_intencion_de_venta: datos_array[0].strip.to_i
+      id_intencion_de_venta: datos_array[0].strip.to_i,
+      tipo: datos_array[1].strip
     }.to_json
   end
 end
--- spec/helpers/publicacion_parser_spec.rb
+++ spec/helpers/publicacion_parser_spec.rb
@@ -5,7 +5,11 @@ describe 'Publicacion parser' do
   let(:mensaje) { '123,p2p,45000' }
   let(:publicacion) { PublicacionParser.new.a_json(mensaje) }
 
-  it 'Al parsear tengo el dni' do
+  it 'Al parsear tengo el id_intencion_de_venta' do
     expect(JSON.parse(publicacion)['id_intencion_de_venta']).to eq 123
+  end
+
+  it 'Al parsear tengo el tipo' do
+    expect(JSON.parse(publicacion)['tipo']).to eq 'p2p'
   end
 end
