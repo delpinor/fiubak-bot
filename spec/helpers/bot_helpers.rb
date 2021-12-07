aecea188ea724bf8362efa66a260aa0e56c2ec08
Gaston Parente <ggparente95@gmail.com>
2021-12-07 06:52:37 +0000

    refactor: removiendo codigo duplicado de mocks

--- spec/helpers/bot_helpers.rb
+++ spec/helpers/bot_helpers.rb
@@ -5,41 +5,21 @@ require 'web_mock'
 
 require "#{File.dirname(__FILE__)}/../../app/bot_client"
 
-def cuando_envio_un_mensaje(token, message_text)
-  body = { "ok": true, "result": [{ "update_id": 693_981_718,
-                                    "message": { "message_id": 11,
-                                                 "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Nairobi', "last_name": 'Gutter', "username": 'egutter', "language_code": 'en' },
-                                                 "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
-                                                 "date": 1_557_782_998, "text": message_text,
-                                                 "entities": [{ "offset": 0, "length": 6, "type": 'bot_command' }] } }] }
-
-  stub_request(:any, "https://api.telegram.org/bot#{token}/getUpdates")
-    .to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
+def get_updates_mock(token, message_text)
+  body = {
+    "ok": true, "result": [{ "update_id": 693_981_718,
+                             "message": {
+                               "message_id": 11,
+                               "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Nairobi', "last_name": 'Gutter', "username": 'egutter', "language_code": 'en' },
+                               "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
+                               "date": 1_557_782_998, "text": message_text,
+                               "entities": [{ "offset": 0, "length": 6, "type": 'bot_command' }]
+                             } }]
+  }
+  stub_request(:any, "https://api.telegram.org/bot#{token}/getUpdates").to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
 end
 
-def cuando_solicito_la_busqueda_de_publicaciones(token, publicaciones_mock, message_text)
-  body = { "ok": true, "result": [{ "update_id": 693_981_718,
-                                    "message": { "message_id": 11,
-                                                 "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Nairobi', "last_name": 'Gutter', "username": 'egutter', "language_code": 'en' },
-                                                 "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
-                                                 "date": 1_557_782_998, "text": message_text,
-                                                 "entities": [{ "offset": 0, "length": 6, "type": 'bot_command' }] } }] }
-
-  stub_request(:any, "https://api.telegram.org/bot#{token}/getUpdates")
-    .to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
-
-  stub_request(:get, 'https://test.api/publicaciones')
-    .with(
-      headers: {
-        'Accept' => '*/*',
-        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'User-Agent' => 'Faraday v0.15.4'
-      }
-    )
-    .to_return(status: 200, body: publicaciones_mock.to_json, headers: {})
-end
-
-def entonces_obtengo_el_mensaje(token, message_text)
+def send_message_mock(token, message_text)
   body = { "ok": true,
            "result": { "message_id": 12,
                        "from": { "id": 715_612_264, "is_bot": true, "first_name": 'fiuba-memo2-prueba', "username": 'fiuba_memo2_bot' },
@@ -53,16 +33,30 @@ def entonces_obtengo_el_mensaje(token, message_text)
     .to_return(status: 200, body: body.to_json, headers: {})
 end
 
-def cuando_registro_un_usuario(token, message_text)
-  body = { "ok": true, "result": [{ "update_id": 693_981_718,
-                                    "message": { "message_id": 11,
-                                                 "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Nairobi', "last_name": 'Gutter', "username": 'egutter', "language_code": 'en' },
-                                                 "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
-                                                 "date": 1_557_782_998, "text": message_text,
-                                                 "entities": [{ "offset": 0, "length": 6, "type": 'bot_command' }] } }] }
+def cuando_envio_un_mensaje(token, message_text)
+  get_updates_mock(token, message_text)
+end
 
-  stub_request(:any, "https://api.telegram.org/bot#{token}/getUpdates")
-    .to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
+def cuando_solicito_la_busqueda_de_publicaciones(token, publicaciones_mock, message_text)
+  get_updates_mock(token, message_text)
+
+  stub_request(:get, 'https://test.api/publicaciones')
+    .with(
+      headers: {
+        'Accept' => '*/*',
+        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent' => 'Faraday v0.15.4'
+      }
+    )
+    .to_return(status: 200, body: publicaciones_mock.to_json, headers: {})
+end
+
+def entonces_obtengo_el_mensaje(token, message_text)
+  send_message_mock(token, message_text)
+end
+
+def cuando_registro_un_usuario(token, message_text)
+  get_updates_mock(token, message_text)
 
   stub_request(:post, 'https://test.api/usuarios')
     .with(
@@ -78,12 +72,7 @@ def cuando_registro_un_usuario(token, message_text)
 end
 
 def cuando_consulto_el_estado(token, message_text)
-  body = { "ok": true, "result": [{ "update_id": 693_981_718,
-                                    "message": { "message_id": 11,
-                                                 "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Nairobi', "last_name": 'Gutter', "username": 'egutter', "language_code": 'en' },
-                                                 "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
-                                                 "date": 1_557_782_998, "text": message_text,
-                                                 "entities": [{ "offset": 0, "length": 6, "type": 'bot_command' }] } }] }
+  get_updates_mock(token, message_text)
 
   resultado = {
     "mensaje": 'intencion de venta recuperadas con exito',
@@ -99,8 +88,6 @@ def cuando_consulto_el_estado(token, message_text)
       "id": 21
     }
   }
-  stub_request(:any, "https://api.telegram.org/bot#{token}/getUpdates")
-    .to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
   stub_request(:get, 'https://test.api/intenciones_de_venta/21')
     .with(
       headers: {
@@ -113,15 +100,7 @@ def cuando_consulto_el_estado(token, message_text)
 end
 
 def cuando_registro_una_publicacion_por_p2p(token, message_text)
-  body = { "ok": true, "result": [{ "update_id": 693_981_718,
-                                    "message": { "message_id": 11,
-                                                 "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Nairobi', "last_name": 'Gutter', "username": 'egutter', "language_code": 'en' },
-                                                 "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
-                                                 "date": 1_557_782_998, "text": message_text,
-                                                 "entities": [{ "offset": 0, "length": 6, "type": 'bot_command' }] } }] }
-
-  stub_request(:post, "https://api.telegram.org/bot#{token}/getUpdates")
-    .to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
+  get_updates_mock(token, message_text)
 
   stub_request(:post, 'https://test.api/publicaciones')
     .with(
@@ -137,18 +116,11 @@ def cuando_registro_una_publicacion_por_p2p(token, message_text)
 end
 
 def cuando_consulto_el_estado_inexistente(token, message_text)
-  body = { "ok": true, "result": [{ "update_id": 693_981_718,
-                                    "message": { "message_id": 11,
-                                                 "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Nairobi', "last_name": 'Gutter', "username": 'egutter', "language_code": 'en' },
-                                                 "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
-                                                 "date": 1_557_782_998, "text": message_text,
-                                                 "entities": [{ "offset": 0, "length": 6, "type": 'bot_command' }] } }] }
+  get_updates_mock(token, message_text)
 
   resultado = {
     "mensaje": 'intencion de venta no encontrada'
   }
-  stub_request(:any, "https://api.telegram.org/bot#{token}/getUpdates")
-    .to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
   stub_request(:get, 'https://test.api/intenciones_de_venta/-1')
     .with(
       headers: {
@@ -161,15 +133,7 @@ def cuando_consulto_el_estado_inexistente(token, message_text)
 end
 
 def cuando_registro_una_oferta_p2p(token, message_text)
-  body = { "ok": true, "result": [{ "update_id": 693_981_718,
-                                    "message": { "message_id": 11,
-                                                 "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Nairobi', "last_name": 'Gutter', "username": 'egutter', "language_code": 'en' },
-                                                 "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
-                                                 "date": 1_557_782_998, "text": message_text,
-                                                 "entities": [{ "offset": 0, "length": 6, "type": 'bot_command' }] } }] }
-
-  stub_request(:post, "https://api.telegram.org/bot#{token}/getUpdates")
-    .to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
+  get_updates_mock(token, message_text)
 
   stub_request(:post, 'https://test.api/publicaciones/1/ofertas')
     .with(
@@ -185,12 +149,7 @@ def cuando_registro_una_oferta_p2p(token, message_text)
 end
 
 def cuando_consulto_el_estado_de_la_publicacion(token, message_text)
-  body = { "ok": true, "result": [{ "update_id": 693_981_718,
-                                    "message": { "message_id": 11,
-                                                 "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Nairobi', "last_name": 'Gutter', "username": 'egutter', "language_code": 'en' },
-                                                 "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
-                                                 "date": 1_557_782_998, "text": message_text,
-                                                 "entities": [{ "offset": 0, "length": 6, "type": 'bot_command' }] } }] }
+  get_updates_mock(token, message_text)
   # 21, marca: Fiat, modelo: Uno, anio: 1995, patente: 'MHF-200', precio: 75000\nofertas:\n [#1 monto_ofertado: 45000]
   resultado = {
     "id": 21,
@@ -204,9 +163,6 @@ def cuando_consulto_el_estado_de_la_publicacion(token, message_text)
     ]
   }
 
-  stub_request(:any, "https://api.telegram.org/bot#{token}/getUpdates")
-    .to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
-
   stub_request(:get, 'https://test.api/publicaciones/21')
     .with(
       headers: {
@@ -219,19 +175,11 @@ def cuando_consulto_el_estado_de_la_publicacion(token, message_text)
 end
 
 def cuando_consulto_el_estado_de_la_publicacion_inexistente(token, message_text)
-  body = { "ok": true, "result": [{ "update_id": 693_981_718,
-                                    "message": { "message_id": 11,
-                                                 "from": { "id": 141_733_544, "is_bot": false, "first_name": 'Nairobi', "last_name": 'Gutter', "username": 'egutter', "language_code": 'en' },
-                                                 "chat": { "id": 141_733_544, "first_name": 'Emilio', "last_name": 'Gutter', "username": 'egutter', "type": 'private' },
-                                                 "date": 1_557_782_998, "text": message_text,
-                                                 "entities": [{ "offset": 0, "length": 6, "type": 'bot_command' }] } }] }
+  get_updates_mock(token, message_text)
   # 21, marca: Fiat, modelo: Uno, anio: 1995, patente: 'MHF-200', precio: 75000\nofertas:\n [#1 monto_ofertado: 45000]
   resultado = {
     "mensaje": 'La publicacion no existe'
   }
-
-  stub_request(:any, "https://api.telegram.org/bot#{token}/getUpdates")
-    .to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
 
   stub_request(:get, 'https://test.api/publicaciones/42')
     .with(
